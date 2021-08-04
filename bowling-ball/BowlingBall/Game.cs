d5f41a5e248a275b2fa65c1bf1243685b5d4eb45
shrk49 <shrutikak49@gmail.com>
2021-08-04 17:22:34 +0530

    Update Game.cs
    Updated Game.cs final

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -8,15 +8,55 @@ namespace BowlingBall
 {
     public class Game
     {
+           private int[] rolls = new int[21];
+          private int currentRoll = 0;
+          public int score;
+          public int scoringCurrentRoll;
         public void Roll(int pins)
         {
             // Add your logic here. Add classes as needed.
+             rolls[currentRoll++] = pins;
         }
 
         public int GetScore()
         {
             // Returns the final score of the game.
-            return 0;
+            for(int frame = 1; frame <= 10; frame++)
+                {
+                      if(isStrike(scoringCurrentRoll))
+                        addScoreForStrike();
+                      else if(isSpare(scoringCurrentRoll))
+                        addScoreForSpare();
+                      else
+                        addScoreForStandardFrame();
+                }
+                        return score;                    
         }
+        
+           public boolean isSpare(int currentRoll)
+          {
+            return rolls[currentRoll] + rolls[currentRoll+1] == 10;
+          }
+
+          public boolean isStrike(int currentRoll)
+          {
+            return rolls[currentRoll] == 10;
+          }
+        
+           public void addScoreForStrike()
+          {
+            score += 10 + rolls[scoringCurrentRoll + 1] + rolls[scoringCurrentRoll + 2];
+            scoringCurrentRoll += 1; 
+          }
+         public void addScoreForSpare()
+          {
+            score += 10 + rolls[scoringCurrentRoll + 2];
+            scoringCurrentRoll += 2;
+          }
+        public void addScoreForStandardFrame()
+          {
+            score += rolls[scoringCurrentRoll] + rolls[scoringCurrentRoll + 1];
+            scoringCurrentRoll += 2;
+          }
     }
 }
