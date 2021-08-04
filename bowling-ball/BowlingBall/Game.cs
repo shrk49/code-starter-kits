2296b27666cf409a193334888db178324bd53f45
shrk49 <shrutikak49@gmail.com>
2021-08-04 15:25:00 +0530

    Update Game.cs

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -8,15 +8,26 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll=0;
         public void Roll(int pins)
         {
             // Add your logic here. Add classes as needed.
+            rolls[currentRoll++]=pins;
         }
 
         public int GetScore()
         {
             // Returns the final score of the game.
             return 0;
+        }
+         public void Roll(int[] pins)
+        {
+            for(int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
         }
     }
 }
