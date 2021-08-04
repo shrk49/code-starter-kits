87b4032a8579ee41b17c739a98be3a1dc29709b3
shrk49 <shrutikak49@gmail.com>
2021-08-04 18:18:54 +0530

    Update GameFixture.cs
    Updated Tests.cs final

--- bowling-ball/BowlingBall.Tests/GameFixture.cs
+++ bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -6,20 +6,50 @@ namespace BowlingBall.Tests
     [TestClass]
     public class GameFixture
     {
+        private Game game;
+        
         [TestMethod]
-        public void Gutter_game_score_should_be_zero_test()
+        public void CanRoll
         {
-            var game = new Game();
-            Roll(game, 0, 20);
-            Assert.AreEqual(0, game.GetScore());
+            RollThroughMany(0,20);
+            Assert.AreEqual(0,game.GetScore());
         }
-
-        private void Roll(Game game, int pins, int times)
+        [TestMethod]
+        public void CanRollAll()
         {
-            for (int i = 0; i < times; i++)
-            {
-                game.Roll(pins);
-            }
+            RollThroughMany(1,20);
+            Assert.AreEqual(20,game.GetScore());
         }
+        private void RollThroughMany(int pins,int rolls)
+        {
+            for(var i=0;i<rolls;i++)
+            game.Roll(pins);
+        }
+        [TestMethod]
+        public void CanRollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollThroughMany(0,17);
+            Assert.AreEqual(16,game.GetScore());
+        }
+        [TestMethod]
+        public void CanRollStrike()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+            RollThroughMany(0,16);
+            Assert.AreEqual(24,game.GetScore());
+        }
+        [TestMethod]
+        public void CanRollComplete()
+        {
+            RollThroughMany(10,12);
+            Assert.AreEqual(300,game.GetScore());
+        }
+        
+      
     }
 }
